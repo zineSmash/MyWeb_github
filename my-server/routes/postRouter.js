ed9bdd8db3fc6a6c14cf36089304ee9d80f5b9cf
mac <mac@zinemac.local>
2025-06-06 17:37:27 +0900

    게시글 리스트에 삭제 버튼 추가

--- my-server/routes/postRouter.js
+++ my-server/routes/postRouter.js
@@ -63,6 +63,39 @@ router.get('/', async (req, res) => {
     }
 });
 
+
+// 게시글 삭제 
+router.delete('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(!post) return res.status(404).json({ message: '게시글을 찾을 수 없습니다.'});
+
+        // 이미지들 삭제
+        for (const imagePath of post.images) {
+            if (imagePath.startsWith('https://res.cloudinary.com/')) {
+                // Cloudinary 이미지
+                const publicId = imagePath.split('/').slice(-2).join('/').split('.')[0]; // 예: webtoon_uploads/abcd1234
+                await cloudinary.uploader.destroy(publicId);
+            } else if (imagePath.startsWith('/uploads/')) {
+                // 로컬 이미지
+                const localPath = path.join(__dirname, '..', imagePath);
+                fs.unlink(localPath, err => {
+                    if (err) console.error('로컬 파일 삭제 실패:', err);
+                });
+            }
+        }
+
+        // 게시글 삭제
+        await Post.findByIdAndDelete(req.params.id);
+
+        res.json({ message: '게시글과 이미지가 성공적으로 삭제되었습니다.' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: '게시글 삭제 중 오류 발생' });
+    }
+});
+
+
 module.exports = router;
 
 // // uploads 경로 설정
